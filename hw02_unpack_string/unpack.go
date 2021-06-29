c7f4a66155ef3414e78e33395c74cae6528f262b
Spigell <spigelly@gmail.com>
2021-06-30 01:00:51 +0300

    :recycle: another way

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,31 +13,31 @@ func Unpack(str string) (string, error) {
 	var builder strings.Builder
 
 	builder.WriteString("")
+	_, lastSymbolSize := utf8.DecodeLastRuneInString(str)
 
 	var previousSymbol rune
-	for i, symbol := range str {
+	for pos, symbol := range str {
 		// symbol is a rune
 
-		if unicode.IsDigit(symbol) {
-			if unicode.IsDigit(previousSymbol) || i == 0 {
+		switch {
+		case unicode.IsDigit(symbol):
+			if unicode.IsDigit(previousSymbol) || pos == 0 {
 				// "3abc" is invalid (starts with digit)
 				// "aaa10b", "45" are invalid (digit before digit, eg. a number)
 				return "", ErrInvalidString
 			}
 
-			if digitRuneToInt(symbol) == 0 && !unicode.IsDigit(previousSymbol) {
-				// Do not write to builder if symbol is Zero (aaa0b -> aab)
-				continue
-			}
 			// Repeat a previous symbol
-			builder.WriteString(strings.Repeat(string(previousSymbol), digitRuneToInt(symbol)-1))
-		} else {
-			if utf8.RuneCountInString(str)-1 > i && rune(str[i+1]) == '0' {
-				// Do not write to builder if next symbol is Zero (aaa0b -> aab)
-				continue
+			builder.WriteString(strings.Repeat(string(previousSymbol), digitRuneToInt(symbol)))
+
+		case len(str)-lastSymbolSize == pos:
+			if !unicode.IsDigit(previousSymbol) {
+				builder.WriteRune(previousSymbol)
 			}
-			// Write a symbol
 			builder.WriteRune(symbol)
+
+		case !unicode.IsDigit(previousSymbol) && pos != 0:
+			builder.WriteRune(previousSymbol)
 		}
 		previousSymbol = symbol
 	}
